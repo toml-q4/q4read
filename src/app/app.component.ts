10f0b164d30b20538e744db9599d280e4bbd69d2
Tom Luong <toml@q4inc.com>
2017-04-22 23:54:34 -0400

    add config service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import { FrameworkConfigService } from '../fw/services/framework-config.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  constructor(private framworkConfigService: FrameworkConfigService)
+  {
+
+  }
 }
