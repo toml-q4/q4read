a99cfd41898c05e5c7fd7ef9084fc5f3f11aad8c
Tom Luong <toml@q4inc.com>
2017-04-22 23:14:32 -0400

    q4read

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-
+import { FwModule } from '../fw/fw.module'
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +12,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    FwModule
   ],
   providers: [],
   bootstrap: [AppComponent]
